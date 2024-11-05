5ece41c93ce5bda295808eea3d024f6df4e5371e
Owen Rombawa <rombawao@gmail.com>
2024-11-05 11:47:54 -0800

    async edit

--- async/async.js
+++ async/async.js
@@ -1,15 +1,15 @@
-const doFetch = () => {
-//const doFetch = async () => {
+//const doFetch = () => {
+const doFetch = async () => {
     console.log("fetching from API...")
     const startTime = Date.now();
 
-    const response = fetch('https://fakestoreapi.com/products?limit=1')
-    .then(response => {
+    const response = await fetch('https://fakestoreapi.com/products?limit=1')
+    
         let elapsedTime = Date.now() - startTime;
         console.log(`time elapsed since start for query: ${elapsedTime} ms`);
         console.log(`response:`);
         console.log(response);    
-    })
+    
     //const response = await fetch('https://fakestoreapi.com/products?limit=1');
 
 
